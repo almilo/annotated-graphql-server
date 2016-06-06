484d3f5d92812a8ae38d3d892967758bcc04963e
Alberto Mijares <almilo@almilo.es>
2016-06-06 14:35:29 +0200

    (refactor): extract endpoint options creation

--- src/dispatcher/graphql-endpoint-dispatcher-factory.js
+++ src/dispatcher/graphql-endpoint-dispatcher-factory.js
@@ -3,16 +3,13 @@ import { AnnotatedGraphQLSchemaFactory } from 'annotated-graphql';
 import schemaFactory from './schema-factory';
 
 export default function (baseEndpointUrlPath, annotatedSchemas, schemaAnnotationExtractors) {
-    const apisGraphQLEndpoint = graphqlHttp({
-            schema: schemaFactory(annotatedSchemas, baseEndpointUrlPath),
-            pretty: true,
-            graphiql: true
-        }),
+    const schemasSchema = schemaFactory(annotatedSchemas, baseEndpointUrlPath),
+        schemasEndpoint = graphqlHttp(createEndpointOptions(schemasSchema)),
         annotatedGraphQLSchemaFactory = new AnnotatedGraphQLSchemaFactory(schemaAnnotationExtractors),
         apiEndpoints = Object.keys(annotatedSchemas).reduce(toApiEndpoint, {});
 
     return function (req, res) {
-        const api = req.params.api, graphqlEndpoint = !api ? apisGraphQLEndpoint : apiEndpoints[api];
+        const api = req.params.api, graphqlEndpoint = !api ? schemasEndpoint : apiEndpoints[api];
 
         if (!graphqlEndpoint) {
             res.status(404).send(`No schema for api: '${api}' found.`)
@@ -22,12 +19,18 @@ export default function (baseEndpointUrlPath, annotatedSchemas, schemaAnnotation
     };
 
     function toApiEndpoint(apiEndpoints, apiKey) {
-        apiEndpoints[apiKey] = graphqlHttp({
-            schema: annotatedGraphQLSchemaFactory.createSchema(annotatedSchemas[apiKey]),
-            pretty: true,
-            graphiql: true
-        });
+        const apiSchema = annotatedGraphQLSchemaFactory.createSchema(annotatedSchemas[apiKey]);
+        
+        apiEndpoints[apiKey] = graphqlHttp(createEndpointOptions(apiSchema));
 
         return apiEndpoints
     }
+}
+
+function createEndpointOptions(schema) {
+    return {
+        schema,
+        pretty: true,
+        graphiql: true
+    };
 }
